674e2cb6de372b45f9133da3422dd89d8a95bde1
mmeendez8 <miguelmndez@gmail.com>
2020-04-15 18:28:17 +0200

    Add first simple test

---
+++ tests/coco/test_utils.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+import pytest
+
+from pyodi.coco.utils import get_bbox_matrix, scale_bbox_dimensions
+
+
+@pytest.fixture
+def get_simple_annotations_with_img_sizes():
+    def _get_fake_data():
+        bboxes = np.array([[0, 0, 10, 10, 100, 100], [25, 25, 50, 50, 100, 100]])
+        columns = [
+            "col_centroid",
+            "row_centroid",
+            "width",
+            "height",
+            "img_width",
+            "img_height",
+        ]
+        return pd.DataFrame(bboxes, columns=columns)
+
+    return _get_fake_data
+
+
+def test_scale_bbox_dimensions(get_simple_annotations_with_img_sizes):
+    df_annotations = get_simple_annotations_with_img_sizes()
+    df_annotations = scale_bbox_dimensions(df_annotations, (1280, 720))
+    bboxes = get_bbox_matrix(df_annotations, prefix="scaled")
+    expected_bboxes = np.array([[0, 0, 128, 72], [320, 180, 640, 360]], dtype=np.int32)
+    np.testing.assert_equal(bboxes, expected_bboxes)
