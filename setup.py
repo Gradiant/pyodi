70d5e55c9331f74e450cfc718731f02a8d517321
mmeendez8 <miguelmndez@gmail.com>
2020-04-17 13:03:49 +0200

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_namespace_packages, setup
+
+MODULE_NAME = "pyodi"
+PACKAGE_NAME = "pyodi"
+VERSION = "0.0"
+
+setup(
+    name=PACKAGE_NAME,
+    version=VERSION,
+    packages=find_namespace_packages(
+        include="{}.*".format(MODULE_NAME), exclude=["tests", "logs"]
+    ),
+    include_package_data=True,
+    platforms="any",
+    python_requires=">=3.6",
+)
