133688868833dd2c79ca9002783b2ac40321fa53
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2020-04-28 10:20:36 +0200

    Update docsting for rendering

--- pyodi/apps/train_config_evaluation.py
+++ pyodi/apps/train_config_evaluation.py
@@ -56,23 +56,23 @@ def train_config_evaluation(
     train_config_file: str
         Path to MMDetection-like configuration file.
         Must contain `train_pipeline` and `anchor_generator` sections.
-
         Example content:
 
-            ```python
-            # faster_rcnn_r50_fpn_gn_ws_config.py
-            train_pipeline = [
-                dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
-                dict(type='RandomFlip', flip_ratio=0.5),
-                dict(type='Pad', size_divisor=32)
-            ]
-            anchor_generator=dict(
-                type='AnchorGenerator',
-                scales=[8],
-                ratios=[0.5, 1.0, 2.0],
-                strides=[4, 8, 16, 32, 64]
-            )
-            ```
+        # faster_rcnn_r50_fpn_gn_ws_config.py
+
+        train_pipeline = [
+            dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+            dict(type='RandomFlip', flip_ratio=0.5),
+            dict(type='Pad', size_divisor=32)
+        ]
+
+        anchor_generator=dict(
+            type='AnchorGenerator',
+            scales=[8],
+            ratios=[0.5, 1.0, 2.0],
+            strides=[4, 8, 16, 32, 64]
+        )
+
     show : bool, optional
         Show results or not, by default True
     output : str, optional
