643221f6fa134ae48e0f81a4397ed26a4b757aa5
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2020-04-28 10:06:22 +0200

    Update cli with train_config_app

--- pyodi/cli.py
+++ pyodi/cli.py
@@ -2,13 +2,13 @@ import typer
 
 from pyodi.apps.evaluation import evaluation
 from pyodi.apps.ground_truth import ground_truth
-from pyodi.apps.train_config import train_config
+from pyodi.apps.train_config import train_config_app
 
 app = typer.Typer()
 
 app.command()(evaluation)
 app.command()(ground_truth)
-app.command()(train_config)
+app.add_typer(train_config_app, name="train-config")
 
 
 if __name__ == "__main__":
