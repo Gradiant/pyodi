97f3cc9aa214175b1db556ec2bec8b5e41b4878e
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2020-04-28 10:08:59 +0200

    Update docstring

--- pyodi/apps/train_config_evaluation.py
+++ pyodi/apps/train_config_evaluation.py
@@ -54,8 +54,25 @@ def train_config_evaluation(
     ground_truth_file : str
         Path to COCO ground truth file
     train_config_file: str
-        Path to MMDetection-lile configuration file -
-        With `train_pipeline` and `anchor_generator` sections.
+        Path to MMDetection-like configuration file.
+        Must contain `train_pipeline` and `anchor_generator` sections.
+
+        Example content:
+
+            ```python
+            # faster_rcnn_r50_fpn_gn_ws_config.py
+            train_pipeline = [
+                dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
+                dict(type='RandomFlip', flip_ratio=0.5),
+                dict(type='Pad', size_divisor=32)
+            ]
+            anchor_generator=dict(
+                type='AnchorGenerator',
+                scales=[8],
+                ratios=[0.5, 1.0, 2.0],
+                strides=[4, 8, 16, 32, 64]
+            )
+            ```
     show : bool, optional
         Show results or not, by default True
     output : str, optional
