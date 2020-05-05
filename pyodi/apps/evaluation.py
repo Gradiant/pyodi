2edfa13b0fa1d8d52b764e6f3d586d4b2461caa1
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2020-05-05 13:33:50 +0200

    Feature/issue 3  apps evaluation add f1 io u metric (#11)
    * Add per_category_stats
    
    * Add F1 and LRP metrics
    
    * Add F1 and LRP metrics
    
    * Remove print
    
    * Use arange
    
    * Use np.where
    
    * Apply suggestions from code review
    
    Co-authored-by: Miguel Mendez <miguelmndez@gmail.com>
    
    Co-authored-by: Miguel Mendez <miguelmndez@gmail.com>

--- pyodi/apps/evaluation.py
+++ pyodi/apps/evaluation.py
@@ -14,15 +14,13 @@ app = typer.Typer()
 @logger.catch
 @app.command()
 def evaluation(
-    ground_truth_file: str,
-    predictions_file: str,
-    string_to_match: Optional[str] = None,
+    ground_truth_file: str, predictions_file: str, string_to_match: Optional[str] = None
 ):
 
     coco_ground_truth = load_coco_ground_truth_from_StringIO(open(ground_truth_file))
     coco_predictions = coco_ground_truth.loadRes(json.load(open(predictions_file)))
 
-    coco_eval = COCOeval(coco_ground_truth, coco_predictions, "bbox")
+    coco_eval = COCOeval(coco_ground_truth, coco_predictions)
 
     if string_to_match is not None:
         filtered_ids = [
@@ -34,24 +32,10 @@ def evaluation(
     else:
         filtered_ids = [k for k in coco_ground_truth.imgs.keys()]
 
-    coco_eval.params.imgIds = filtered_ids
+    coco_eval.image_ids = filtered_ids
     coco_eval.evaluate()
     coco_eval.accumulate()
     coco_eval.summarize()
-
-    metric_names = [
-        "Average Precision  (AP) @[ IoU=0.50:0.95 | area=   all | maxDets=100 ]",
-        "Average Precision  (AP) @[ IoU=0.50      | area=   all | maxDets=100 ]",
-        "Average Precision  (AP) @[ IoU=0.75      | area=   all | maxDets=100 ]",
-        "Average Precision  (AP) @[ IoU=0.50:0.95 | area= small | maxDets=100 ]",
-        "Average Precision  (AP) @[ IoU=0.50:0.95 | area=medium | maxDets=100 ]",
-        "Average Precision  (AP) @[ IoU=0.50:0.95 | area= large | maxDets=100 ]",
-    ]
-    eval_results = {}
-    for n, metric_name in enumerate(metric_names):
-        eval_results[metric_name] = float("{:.3f}".format(coco_eval.stats[n]))
-
-    logger.info(eval_results)
 
 
 if __name__ == "__main__":
