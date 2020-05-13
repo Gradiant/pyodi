74a40c38aafb4f00e8fd7dea5667a259ac1cb98f
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2020-05-13 13:16:47 +0200

    Add crops merge (#40)
    * Add crops_join
    
    * Move nms to pyodi/core
    
    * Fix import
    
    * Remove empty file
    
    * Use ensemble_boxes
    
    * Use coco utils
    
    * Remove commented test
    
    * Use shape

--- pyodi/cli.py
+++ pyodi/cli.py
@@ -1,5 +1,6 @@
 import typer
 
+from pyodi.apps.crops import crops_app
 from pyodi.apps.evaluation import evaluation
 from pyodi.apps.ground_truth import ground_truth
 from pyodi.apps.train_config import train_config_app
@@ -9,6 +10,7 @@ app = typer.Typer()
 app.command()(evaluation)
 app.command()(ground_truth)
 app.add_typer(train_config_app, name="train-config")
+app.add_typer(crops_app, name="crops")
 
 
 if __name__ == "__main__":
