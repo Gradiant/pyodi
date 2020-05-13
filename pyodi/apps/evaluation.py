c1fa32e31a646fce053a5f4747019ed3d64d7901
Iago GR <igonro@gmail.com>
2020-05-13 09:42:41 +0200

    Add docs for evaluation

--- pyodi/apps/evaluation.py
+++ pyodi/apps/evaluation.py
@@ -1,3 +1,35 @@
+"""
+# Evaluation App
+
+The [`pyodi evaluation`][pyodi.apps.evaluation.evaluation] app can be used to evaluate the predictions of an object detection dataset.
+
+Example usage:
+
+``` bash
+pyodi evaluation "data/COCO/COCO_val2017.json" "data/COCO/COCO_val2017_predictions.json"
+```
+
+This app shows the Average Precision for different IoU values and different areas, the Average Recall for different IoU values and differents maximum detections, and the Average F1 Score.
+
+An example of the result of executing this app:
+```
+ Average Precision                        (AP) @[ IoU=0.50:0.95 | area=   all | maxDets=100 ] = 0.257
+ Average Precision                        (AP) @[ IoU=0.50      | area=   all | maxDets=100 ] = 0.439
+ Average Precision                        (AP) @[ IoU=0.75      | area=   all | maxDets=100 ] = -1.000
+ Average Precision                        (AP) @[ IoU=0.50:0.95 | area= small | maxDets=100 ] = 0.069
+ Average Precision                        (AP) @[ IoU=0.50:0.95 | area=medium | maxDets=100 ] = 0.277
+ Average Precision                        (AP) @[ IoU=0.50:0.95 | area= large | maxDets=100 ] = 0.426
+ Average Recall                           (AR) @[ IoU=0.50:0.95 | area=   all | maxDets=  1 ] = 0.240
+ Average Recall                           (AR) @[ IoU=0.50:0.95 | area=   all | maxDets= 10 ] = 0.354
+ Average Recall                           (AR) @[ IoU=0.50:0.95 | area=   all | maxDets=100 ] = 0.376
+ Average Recall                           (AR) @[ IoU=0.50:0.95 | area= small | maxDets=100 ] = 0.124
+ Average Recall                           (AR) @[ IoU=0.50:0.95 | area=medium | maxDets=100 ] = 0.416
+ Average Recall                           (AR) @[ IoU=0.50:0.95 | area= large | maxDets=100 ] = 0.585
+ Average F1                               (F1) @[ IoU=0.50      | area=   all | maxDets=100 ] = 0.761
+```
+
+# API REFERENCE
+"""
 import json
 import re
 from typing import Optional
@@ -16,6 +48,14 @@ app = typer.Typer()
 def evaluation(
     ground_truth_file: str, predictions_file: str, string_to_match: Optional[str] = None
 ):
+    """
+    Evaluate the predictions of a dataset.
+
+    Args:
+        ground_truth_file: Path to COCO ground truth file.
+        predictions_file: Path to COCO predictions file.
+        string_to_match: If not None, only images whose file_name match this parameter will be evaluated.
+    """
 
     coco_ground_truth = load_coco_ground_truth_from_StringIO(open(ground_truth_file))
     coco_predictions = coco_ground_truth.loadRes(json.load(open(predictions_file)))
