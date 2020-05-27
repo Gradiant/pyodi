007784b52ef4148ffebd73d52a0ba77dc2494f88
David de la Iglesia Castro <daviddelaiglesiacastro@gmail.com>
2020-05-27 13:46:44 +0200

    Add paint_annotations (#59)
    * Add paint_annotations
    
    * Replace old_image with image

--- pyodi/cli.py
+++ pyodi/cli.py
@@ -3,14 +3,16 @@ import typer
 from pyodi.apps.crops import crops_app
 from pyodi.apps.evaluation import evaluation
 from pyodi.apps.ground_truth import ground_truth
+from pyodi.apps.paint_annotations import paint_annotations
 from pyodi.apps.train_config import train_config_app
 
 app = typer.Typer()
 
+app.add_typer(crops_app, name="crops")
 app.command()(evaluation)
 app.command()(ground_truth)
+app.command()(paint_annotations)
 app.add_typer(train_config_app, name="train-config")
-app.add_typer(crops_app, name="crops")
 
 
 if __name__ == "__main__":
