e6cf8b6d5ff55d1f92c923a3a176ffa100b538bc
Andrés Campos <andres.campos.cuina@gmail.com>
2020-07-28 19:46:06 +0200

    Allowing only ground truth in merge crops and adding a test for this … (#78)
    * Allowing only ground truth in merge crops and adding a test for this feature
    
    * Fixed bug in merge crops test
    
    * Added requested change
    
    * Moved function `test_crops_merge_gt` to test_merge.py in order to have a single test file per app

--- pyodi/apps/crops_merge.py
+++ pyodi/apps/crops_merge.py
@@ -1,5 +1,6 @@
 import json
 from pathlib import Path
+from typing import Optional
 
 import typer
 from loguru import logger
@@ -13,20 +14,21 @@ app = typer.Typer()
 @app.command()
 def crops_merge(
     ground_truth_file: str,
-    predictions_file: str,
     output_file: str,
+    predictions_file: Optional[str] = None,
     apply_nms: bool = True,
     nms_mode: str = "nms",
     score_thr: float = 0.0,
     iou_thr: float = 0.5,
 ) -> str:
-    """Merge and translate `predictions` to `ground_truth`'s `old_images` coordinates.
+    """Merge and translate `ground_truth_file` or `predictions` to `ground_truth`'s `old_images` coordinates.
 
     Args:
-        ground_truth_file : Path to COCO ground truth file of crops. Generated with
+        ground_truth_file: Path to COCO ground truth file of crops. Generated with
             `crops_split`.
-        predictions_file:Path to COCO predictions file over `ground_truth_file`.
-        output_file: Path where the merged predictions will be saved.
+        output_file: Path where the merged annotations will be saved.
+        predictions_file: Path to COCO predictions file over `ground_truth_file`.
+            If not None, the annotations of predictions_file will be merged instead of ground_truth_file's.
         apply_nms: Whether to apply Non Maximum Supression to the merged predictions of
             each image. Defaults to True.
         nms_mode: Non Maximum Supression mode. Defaults to "nms".
@@ -48,9 +50,12 @@ def crops_merge(
         for x in ground_truth["old_images"]
     }
 
-    predictions = json.load(open(predictions_file))
+    if predictions_file is not None:
+        annotations = json.load(open(predictions_file))
+    else:
+        annotations = ground_truth["annotations"]
 
-    for n, crop in enumerate(predictions):
+    for n, crop in enumerate(annotations):
         if not n % 10:
             logger.info(n)
         filename = crop_id_to_filename[crop["image_id"]]
@@ -69,16 +74,27 @@ def crops_merge(
         crop["original_image_shape"] = stem_to_original_shape[stem]
 
     if apply_nms:
-        predictions = nms_predictions(
-            predictions, score_thr=score_thr, nms_mode=nms_mode, iou_thr=iou_thr
+        annotations = nms_predictions(
+            annotations, score_thr=score_thr, nms_mode=nms_mode, iou_thr=iou_thr
         )
         output_file = str(
             Path(output_file).parent
             / f"{Path(output_file).stem}_{nms_mode}_{score_thr}_{iou_thr}.json"
         )
 
+    if predictions_file is not None:
+        new_ground_truth = annotations
+    else:
+        new_ground_truth = {
+            "info": ground_truth.get("info", []),
+            "licenses": ground_truth.get("licenses", []),
+            "categories": ground_truth.get("categories", []),
+            "images": ground_truth.get("old_images"),
+            "annotations": annotations,
+        }
+
     with open(output_file, "w") as f:
-        json.dump(predictions, f, indent=2)
+        json.dump(new_ground_truth, f, indent=2)
 
     return output_file
 
