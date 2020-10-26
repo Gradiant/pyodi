8e52fbb4cc781bb1868c28ff3a587bf83bc78e87
Iago GR <igonro@gmail.com>
2020-10-26 10:32:06 +0100

    Fix minor issues

--- pyodi/apps/coco_merge.py
+++ pyodi/apps/coco_merge.py
@@ -42,23 +42,12 @@ def coco_merge(input_file_1: str, input_file_2: str, output_file: str,) -> str:
     with open(input_file_2, "r") as f:
         data_2 = json.load(f)
 
-    output = deepcopy(data_1)
+    output = {}
+    for k, v in data_1.items():
+        if k not in ("images", "annotations"):
+            output[k] = deepcopy(v)
+
     output["images"], output["annotations"] = [], []
-
-    cat_id_map = {}
-    for new_cat in data_2["categories"]:
-        new_id = None
-        for output_cat in output["categories"]:
-            if new_cat["name"] == output_cat["name"]:
-                new_id = output_cat["id"]
-                break
-
-        if new_id is not None:
-            cat_id_map[new_cat["id"]] = new_id
-        else:
-            cat_id_map[new_cat["id"]] = max(c["id"] for c in output["categories"]) + 1
-            new_cat["id"] = max(c["id"] for c in output["categories"]) + 1
-            output["categories"].append(new_cat)
 
     for i, data in enumerate([data_1, data_2]):
 
@@ -67,6 +56,22 @@ def coco_merge(input_file_1: str, input_file_2: str, output_file: str,) -> str:
                 i + 1, len(data["images"]), len(data["annotations"])
             )
         )
+
+        cat_id_map = {}
+        for new_cat in data["categories"]:
+            new_id = None
+            for output_cat in output["categories"]:
+                if new_cat["name"] == output_cat["name"]:
+                    new_id = output_cat["id"]
+                    break
+
+            if new_id is not None:
+                cat_id_map[new_cat["id"]] = new_id
+            else:
+                new_cat_id = max(c["id"] for c in output["categories"]) + 1
+                cat_id_map[new_cat["id"]] = new_cat_id
+                new_cat["id"] = new_cat_id
+                output["categories"].append(new_cat)
 
         img_id_map = {}
         for image in data["images"]:
@@ -79,8 +84,7 @@ def coco_merge(input_file_1: str, input_file_2: str, output_file: str,) -> str:
         for annotation in data["annotations"]:
             annotation["id"] = n_anns
             annotation["image_id"] = img_id_map[annotation["image_id"]]
-            if i:
-                annotation["category_id"] = cat_id_map[annotation["category_id"]]
+            annotation["category_id"] = cat_id_map[annotation["category_id"]]
 
             n_anns += 1
             output["annotations"].append(annotation)
