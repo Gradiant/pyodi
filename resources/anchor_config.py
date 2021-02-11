80b08bc868944ccd5b60982a3836a1fc5bcca947
Miguel Méndez <miguelmndez@gmail.com>
2021-02-11 10:25:10 +0100

    Issue 106 update readme (#119)
    * Add initial README style
    
    * Add paint sample image
    
    * Add ground truth exploration
    
    * Add train config generation
    
    * Update images and Readme with COCO animals
    
    * Multiple fixes:
    - Rename base anchors
    - Update axis names
    - Add np transform
    
    * Add draft train config evalution
    
    * Update train config
    
    * Multiple fixes:
    - Rename base anchors
    - Add np transform
    
    * Fix lint problems
    
    * Remove print statements
    
    * Add plot and comments to evaluation section
    
    * Add introduction table
    
    * Add test section
    
    * Add coco animals image
    
    * Resize coco animals image

---
+++ resources/anchor_config.py
@@ -0,0 +1,7 @@
+anchor_generator = dict(
+    type="AnchorGenerator",
+    scales=[1.12, 3.13, 8.0],
+    ratios=[0.33, 0.67, 1.4],
+    strides=[4, 8, 16, 32, 64],
+    base_sizes=[4, 8, 16, 32, 64],
+)
